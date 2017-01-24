34dc6d694a241b17182703d468a06b29f530de96
Micke Nordin <mik@elnord.in>
2017-01-24 13:31:17 +0100

    See if we can remove html enteties again

--- lib/Thruk/Controller/api_conf.pm
+++ lib/Thruk/Controller/api_conf.pm
@@ -103,9 +103,16 @@ sub api_call {
 	#if ($payload =~ m/.+/ ) {
 	if ($payload) {
 		print "Payload before" . $payload;
-		$payload =~ s/^\s+//;
-		print "Payload after" . $payload;
-		$req->add_content($payload);
+		my $json_text;
+		open my $fh, '<', \$payload or die $!;
+		while (<$fh>) {
+			$json_text .= s/^\s+// $_;
+		}
+		close $fh;
+		
+		print "Payload after" . $json_text;
+		#$req->add_content($payload);
+		$req->add_content($json_text);
 	}
 	my $response = $ua->request($req);
 	return decode_json $response->decoded_content;
