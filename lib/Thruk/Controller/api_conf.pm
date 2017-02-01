2aafaa5b24ebd7a6a33075652fd7db38b524f0d0
Micke Nordin <mik@elnord.in>
2017-02-01 13:41:15 +0100

    some debug

--- lib/Thruk/Controller/api_conf.pm
+++ lib/Thruk/Controller/api_conf.pm
@@ -170,6 +170,10 @@ sub display_api_response {
     my $result = $q->p("Result from API was:");
     $result .= $q->p( $arr[0]{results}[0]{status} );
     $result .= $q->p( $arr[0]{results}[0]{errors} );
+    unless($arr[0]{results}[0]{status} and $arr[0]{results}[0]{errors}  ){
+        my $result = $q->p("Something unexpected happened.");
+        $result .= $q->p( Dumper $arr[0]{results}[0]);
+    }
     if ( $payload =~ m/.+/ ) {
         $result .= $q->p("Payload was: $payload");
     }
