b20e2d4f50d949de163849ebe504b04cae05a6f3
Micke Nordin <mik@elnord.in>
2017-02-09 12:33:41 +0100

    fix selection

--- lib/Thruk/Controller/api_conf.pm
+++ lib/Thruk/Controller/api_conf.pm
@@ -1471,16 +1471,12 @@ sub contacts {
         push @groups, $element->{'attrs'}{'name'};
     }
 
-    my $period_hash =
-      api_call( $c->stash->{'confdir'}, "GET", "objects/timeperiods" );
-    my @periods;
-    foreach my $element ( $period_hash->{'results'} ) {
-        push @periods, $element->{'attrs'}{'name'};
-    }
-    print "Periods: ";
-    print Dumper @periods;
-    print "Groups: ";
-    print Dumper @groups;
+#    my $period_hash =
+#      api_call( $c->stash->{'confdir'}, "GET", "objects/timeperiods" );
+#    my @periods;
+#    foreach my $element ( $period_hash->{'results'} ) {
+#        push @periods, $element->{'attrs'}{'name'};
+#    }
 
     my $contacts_page =
       '<div class="reportSelectTitle" align="center">Contacts</div>';
@@ -1512,9 +1508,15 @@ sub contacts {
             $contacts_page .= $q->p('Enter email address:');
             $contacts_page .= $q->textarea( 'email', '', 20, 50 );
             $contacts_page .= $q->p('Select user groups:');
+            $contacts_page .=
+                "<select name='group' id='group-select' multiple='multiple'>\n";
+            for my $group (@groups) {
+                $contacts_page .= "<option value=\"$group\">$group</option>\n";
+            }
+            $contacts_page .= "</select>\n";
             $contacts_page .= display_multi_select( "group-select",  @groups );
-            $contacts_page .= $q->p('Select timeperiods:');
-            $contacts_page .= display_multi_select( "period-select", @periods );
+#            $contacts_page .= $q->p('Select timeperiods:');
+#            $contacts_page .= display_multi_select( "period-select", @periods );
             $contacts_page .= $q->hidden( 'page_type', "contacts" );
             $contacts_page .= $q->hidden( 'mode',      "create" );
             $contacts_page .= $q->submit(
